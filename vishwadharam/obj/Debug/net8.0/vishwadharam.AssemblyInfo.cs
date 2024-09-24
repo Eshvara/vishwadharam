cdd2cb4660b89379c91536b3416506c7ad01952e
Eshvara Govender <ST10077567@vcconnect.edu.za>
2024-09-24 20:46:59 +0200

    LAYOUT
    Added the Login button

--- vishwadharam/obj/Debug/net8.0/vishwadharam.AssemblyInfo.cs
+++ vishwadharam/obj/Debug/net8.0/vishwadharam.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vishwadharam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0bc527c9376ab885293b3164e79ea231323fba")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3621bd7e3e5359b20fedfcab19e041d7424a9e")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("vishwadharam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vishwadharam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
