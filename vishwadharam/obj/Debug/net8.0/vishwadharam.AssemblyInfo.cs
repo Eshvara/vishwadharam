086c4bce6220e7bd4a122c7e2877075d41902631
Eshvara Govender <ST10077567@vcconnect.edu.za>
2024-09-24 20:24:12 +0200

    AccountController and Login View
    Created a new controller called AccountController that handles login, logout and access control. The Login View is the login form

--- vishwadharam/obj/Debug/net8.0/vishwadharam.AssemblyInfo.cs
+++ vishwadharam/obj/Debug/net8.0/vishwadharam.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vishwadharam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0bc527c9376ab885293b3164e79ea231323fba")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf792b7ed24202ab5422b9d423c108ea106b42d")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("vishwadharam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vishwadharam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
